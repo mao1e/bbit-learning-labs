01cd76c47d64eb485fa45a81cf987bbf2d334a20
ayebiyi2007 <109806311+ayebiyi2007@users.noreply.github.com>
2026-04-17 01:15:25 +0000

    Sure

--- tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/consumer/solution/consumer_sol.py
@@ -3,13 +3,11 @@ from consumer_interface import mqConsumerInterface
 class mqConsumer(mqConsumerInterface):
     def __init__(self, binding_key: str, exchange_name: str, queue_name: str) -> None:
         # body of constructor
-        
-
-    def setupRMQConnection(self) -> None:
-        con_params = pika.URLParameters(os.environ["AMQP_URL"])
-        connection = pika.BlockingConnection(parameters=con_params)
-
-        channel = connection.channel()
+        self.binding_Key = binding_key
+        self.exchange_name = exchange_name
+        self.queue_name = queue_name
+        self.setupRMQConnection()
+    
 
 
 
